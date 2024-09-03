8e73c296a7dfd39a844264b31db09e7b5a063438
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-09-03 12:58:39 -0300

    Feat Função Perfil e Usuários, modificado painel admin para seguir usuarios manualmente. Testando as funcionalidades pelo proprio django admin

--- social/twitter/admin.py
+++ social/twitter/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile #importando o perfil que criamos
 
-# Register your models here.
+# Removendo Groups do Painel Admin
+
+admin.site.unregister(Group)
+
+# Estender Modo Usuário
+# tivemos que colocar class ProfileInline antes
+class ProfileInline(admin.StackedInline):
+  model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+  model = User
+  # Camos de Usuário na pagina administração
+  fields = ["username"]
+  #adicionando o inline que criamos
+  inlines = [ProfileInline]
+
+# cancelar o registro do usuário inicial
+admin.site.unregister(User)
+
+# Registrar novamente e Perfil
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile) comentamos essa seção pq não queremos duas seções distintas perfil e usuario
+
+# Misturando profile e usuario inofrmações no admin
+
+# class ProfileInline(admin.StackedInline):
+#   model = Profile vamos ter que subir essa classe por causa da herança
+
+# agora voltamos nos usuário que removemos os campos e deixamos o nome e vamos adicionar inlines
