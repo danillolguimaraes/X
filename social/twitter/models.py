511b55a0e8004e051b179b98cd48a59fabb66427
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-09-04 20:44:00 -0300

    Feat Montado estrutura de Perfis, lista de perfis, ajustado layout de perfis e lista, ajustado quem segue quem e estilizado

--- social/twitter/models.py
+++ social/twitter/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+#from django.dispatch import receiver caso usassemos o receiver
 
 # Criar o perfil de usuário
 
@@ -11,9 +13,23 @@ class Profile(models.Model):
   # blank = true, você pode ter uma conta mas não precisa seguir ninguém 
   follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
 
+  date_modified = models.DateTimeField(User, auto_now=True)
+
 # toda grande mudança o servidor precisa migrar novamente python manage.py makemigrations
 # feito isso precisamos empurrar nossa migração python manage.py migrate
 
   #adicionamos essa função para não ficar objeto no painel e sim o nome do usuário
   def __str__(self):
     return self.user.username
+# criar um perfil quando um novo usuário se inscrever
+
+#@receiver(post_save, sender=User) poderiamos usar no lugar de post_save.connect
+def create_profile(sender, instance, created, **kwargs):
+  if created:
+    user_profile = Profile(user=instance)
+    user_profile.save()
+    # usuario siga a si mesmo
+    user_profile.follows.set([instance.profile.id])
+    user_profile.save()
+
+post_save.connect(create_profile, sender=User)
