c5dff9ef8698bac4eaab3041e5ef04bbab34f208
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-09-07 12:35:59 -0300

    Feat adicionado botão seguir e deixar de seguir

--- social/twitter/views.py
+++ social/twitter/views.py
@@ -17,6 +17,21 @@ def profile_list(request):
 def profile(request, pk):
   if request.user.is_authenticated:
     profile = Profile.objects.get(user_id=pk)
+
+    #lógica pós formulário seguir
+    if request.method == "POST":
+      # pegar ID atual
+      current_user_profile = request.user.profile
+      # pegar os dados do formulário
+      action = request.POST['follow']
+      # decidir seguir ou não seguir
+      if action == "unfollow":
+        current_user_profile.follows.remove(profile)
+      elif action == "follow":
+        current_user_profile.follows.add(profile)
+      # salvar o perfil
+      current_user_profile.save()
+
     return render(request, 'profile.html',{'profile':profile})
   else:
     messages.success(request, ("Você deve estar logado para visualizar esta página..."))
