8c7a32e27bae21c796f41c34bd19ba16e842b0de
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-09-07 13:16:38 -0300

    Feat Criamos nosso modelo de tweets e já esta funcionando em cada perfil

--- social/twitter/models.py
+++ social/twitter/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 #from django.dispatch import receiver caso usassemos o receiver
 
+# criar modelo post tweets
+
+class Tweet(models.Model):
+  user = models.ForeignKey(
+      User, related_name="tweets",
+      on_delete=models.DO_NOTHING   
+    )
+  body = models.CharField(max_length=200)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return(
+      f"{self.user}"
+      f"({self.created_at:%Y-%m-%d %H:%M}): "
+      f"{self.body}..."
+      )
+
 # Criar o perfil de usuário
 
 class Profile(models.Model):
