ada0a5309eb1874b7aea0d3c7b080fd96a74dcdd
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-09-15 00:28:14 -0300

    Feat finalizado o projeto Twitter

--- social/twitter/views.py
+++ social/twitter/views.py
@@ -85,6 +85,32 @@ def profile(request, pk):
         messages.success(request, ("Você deve estar logado para visualizar esta página..."))
         return redirect('home')
 
+def followers(request, pk):
+    if request.user.is_authenticated:
+        if request.user.id == pk:
+            profiles = Profile.objects.get(user_id=pk)
+            return render(request, 'followers.html', {'profiles': profiles})
+        else:
+            messages.success(request, ("Não é sua página de Perfil"))
+            return redirect('home')
+
+    else:
+        messages.success(request, ("Você deve estar logado para visualizar esta página..."))
+        return redirect('home')
+
+def follows(request, pk):
+    if request.user.is_authenticated:
+        if request.user.id == pk:
+            profiles = Profile.objects.get(user_id=pk)
+            return render(request, 'follows.html', {'profiles': profiles})
+        else:
+            messages.success(request, ("Não é sua página de Perfil"))
+            return redirect('home')
+
+    else:
+        messages.success(request, ("Você deve estar logado para visualizar esta página..."))
+        return redirect('home')
+
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -171,3 +197,69 @@ def tweet_show(request, pk):
     else:
         messages.error(request, "Tweet não existe!")
         return redirect('home')
+
+def delete_tweet(request, pk):
+    if request.user.is_authenticated:
+        tweet = get_object_or_404(Tweet, id=pk)
+        # Verificar se você possuí um tweet
+        if request.user.username == tweet.user.username:
+            #deletando o tweet
+            tweet.delete()
+            messages.success(request, ("Seu Tweet foi deletado com sucesso!"))
+            return redirect(request.META.get("HTTP_REFERER"))  
+        else:
+            messages.success(request, ("Você não é o dono deste Tweet!"))
+            return redirect('home') 
+    else:
+        messages.success(request, ("Por favor faça login para continuar..."))
+        return redirect(request.META.get("HTTP_REFERER"))
+
+def edit_tweet(request,pk):
+    if request.user.is_authenticated:
+        
+        tweet = get_object_or_404(Tweet, id=pk)
+        # Verificar se você possuí um tweet
+        if request.user.username == tweet.user.username:
+            form = TweetForm(request.POST or None, instance=tweet)
+            if request.method == "POST":
+                if form.is_valid():
+                    tweet_instance = form.save(commit=False)  # Evita conflito com o modelo Tweet
+                    tweet_instance.user = request.user
+                    tweet_instance.save()
+                    messages.success(request, ("Seu Tweet foi editado com sucesso!"))
+                    return redirect(request.META.get("HTTP_REFERER"))
+            else:
+                return render(request, 'edit_tweet.html', {'form':form, 'tweet':tweet})
+        else:
+            messages.success(request, ("Você não é o dono desse Tweet!"))
+            return redirect("home")
+    else:
+        messages.success(request, ("Você precisa estar logado para continuar!"))
+        return redirect("home")
+    
+def search(request):
+    if request.method == "POST":
+        # pegar o campo da pesquisa
+        search = request.POST['search']
+        # procurar no banco de dados
+        searched = Tweet.objects.filter(body__contains = search)
+
+        return render(request, 'search.html', {'search':search, 'searched':searched})
+    else:
+        return render(request, 'search.html', {})
+
+def search_user(request):
+    if request.method == "POST":
+        # Pegar o campo da pesquisa
+        search = request.POST.get('search_user')  # Corrigido para 'search_user'
+        
+        # Procurar no banco de dados
+        searched = User.objects.filter(username__icontains=search)  # Usar 'icontains' para busca case-insensitive
+        
+        return render(request, 'search_user.html', {'search': search, 'searched': searched})
+    else:
+        return render(request, 'search_user.html', {})
+
+            
+   
+            
